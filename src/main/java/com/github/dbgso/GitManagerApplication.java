e2666b9b907ce9d57f5cf2e101d31b5970415a4e
s092018 <s092018@gmail.com>
2017-06-24 10:29:29 +0900

    first commit.

---
+++ src/main/java/com/github/dbgso/GitManagerApplication.java
@@ -0,0 +1,12 @@
+package com.github.dbgso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitManagerApplication.class, args);
+	}
+}
