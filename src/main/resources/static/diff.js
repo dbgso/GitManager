26a0614bbb978c71e8c6700d5d22cd3f1bafa17b
s092018 <s092018@gmail.com>
2017-07-09 19:18:23 +0900

    Refactor diff page.
    Extract javascript from html.

---
+++ src/main/resources/static/diff.js
@@ -0,0 +1,32 @@
+"use strict";
+const param = document.location.search;
+const pathes = document.location.pathname.split('/');
+const repository = pathes[2];
+const sha1 = pathes[3];
+
+const p = repository + "/" + sha1;
+$.ajax({
+    method:'get',
+    url: '/api2/' + p + param
+}).then(function(data){
+    $(document).ready(function () {
+        $('#compare').mergely({
+            cmsettings: {
+                readOnly: true,
+                lineNumbers: true,
+
+            },
+            lhs: function(setValue) {
+                setValue(data[1]);
+            },
+            rhs: function(setValue) {
+                setValue(data[0]);
+            },
+            editor_height: '500px',
+            editor_width: '48%',
+            ignorews: true
+
+
+        });
+    });
+});
