180da46ee4f516ddfa5d3351d7c8f2f362c38c5d
s092018 <s092018@gmail.com>
2017-07-16 23:03:10 +0900

    Fix file read bug.

--- src/main/java/com/github/dbgso/util/StringUtils.java
+++ src/main/java/com/github/dbgso/util/StringUtils.java
@@ -1,21 +1,16 @@
 package com.github.dbgso.util;
 
-import java.io.File;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
 
-import org.apache.tika.detect.EncodingDetector;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
-import org.apache.tika.parser.txt.UniversalEncodingDetector;
 import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
-
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 
 public class StringUtils {
 
@@ -26,7 +21,7 @@ public class StringUtils {
 		ContentHandler handler = new BodyContentHandler(-1);
 		Metadata metadata = new Metadata();
 		try {
-			parser.parse(new ByteInputStream(data, data.length), handler, metadata, new ParseContext());
+			parser.parse(new ByteArrayInputStream(data), handler, metadata, new ParseContext());
 
 			System.out.println(metadata);
 			return handler.toString();
